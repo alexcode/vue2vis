00005d88e151fd10f3dfa8efe5a07764caea13c0
Joao Menighin <joao.menighin@accenture.com>
2018-02-27 08:20:39 -0300

    Updating main.build.js

--- src/main.build.js
+++ src/main.build.js
@@ -1,7 +1,9 @@
 import Timeline from './components/Timeline.vue';
 import Graph2d from './components/Graph2d.vue';
+import Network from './components/Network.vue';
 
 export {
-  Timeline,
-  Graph2d,
+    Timeline,
+    Graph2d,
+    Network
 };
