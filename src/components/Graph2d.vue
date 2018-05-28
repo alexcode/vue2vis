429034962fe3590398d26b2240636d4b6dd6bc70
Alex Couturon <infocontact.alex@gmail.com>
2018-05-28 12:27:53 +0200

    Removing graph2d from observed data

--- src/components/Graph2d.vue
+++ src/components/Graph2d.vue
@@ -5,6 +5,7 @@
 <script>
 import { DataSet, Graph2d } from 'vis';
 
+let graph2d = null;
 const events = [
   'click',
   'contextmenu',
@@ -31,95 +32,92 @@ export default {
       type: Object
     }
   },
-  data: () => ({
-    graph2d: null
-  }),
   watch: {
     items: {
       deep: true,
       handler(n) {
-        this.graph2d.setItems(new DataSet(n));
+        graph2d.setItems(new DataSet(n));
       }
     },
     groups: {
       deep: true,
       handler(v) {
-        this.graph2d.setGroups(new DataSet(v));
+        graph2d.setGroups(new DataSet(v));
       }
     },
     options: {
       deep: true,
       handler(v) {
-        this.graph2d.setOptions(v);
+        graph2d.setOptions(v);
       }
     }
   },
   methods: {
     destroy() {
-      this.graph2d.destroy();
+      graph2d.destroy();
     },
     fit() {
-      this.graph2d.fit();
+      graph2d.fit();
     },
     getCurrentTime() {
-      return this.graph2d.getCurrentTime();
+      return graph2d.getCurrentTime();
     },
     getCustomTime() {
-      return this.graph2d.getCustomTime(id);
+      return graph2d.getCustomTime(id);
     },
     getDataRange() {
-      return this.graph2d.getDataRange();
+      return graph2d.getDataRange();
     },
     getEventProperties(event) {
-      return this.graph2d.getEventProperties(event);
+      return graph2d.getEventProperties(event);
     },
     getLegend(groupId, iconWidth, iconHeight) {
-      return this.graph2d.getLegend(groupId, iconWidth, iconHeight);
+      return graph2d.getLegend(groupId, iconWidth, iconHeight);
     },
     getWindow() {
-      return this.graph2d.getWindow();
+      return graph2d.getWindow();
     },
     isGroupVisible(groupId) {
-      return this.graph2d.isGroupVisible(groupId);
+      return graph2d.isGroupVisible(groupId);
     },
     moveTo(time, options) {
-      this.graph2d.moveTo(time, options);
+      graph2d.moveTo(time, options);
     },
     on(event, callback) {
-      this.graph2d.on(event, callback);
+      graph2d.on(event, callback);
     },
     off(event, callback) {
-      this.graph2d.off(event, callback);
+      graph2d.off(event, callback);
     },
     redraw() {
-      this.graph2d.redraw();
+      graph2d.redraw();
     },
     setCurrentTime(time) {
-      this.graph2d.setCurrentTime(time);
+      graph2d.setCurrentTime(time);
     },
     setCustomTime(time) {
-      this.graph2d.setCustomTime(time);
+      graph2d.setCustomTime(time);
     },
     setGroups(groups) {
-      this.graph2d.setGroups(groups);
+      graph2d.setGroups(groups);
     },
     setItems(items) {
-      this.graph2d.setItems(items);
+      graph2d.setItems(items);
     },
     setOptions(options) {
-      this.graph2d.setOptions(options);
+      graph2d.setOptions(options);
     },
     setWindow(start, end) {
-      this.graph2d.setWindow(start, end);
+      graph2d.setWindow(start, end);
     }
   },
   mounted() {
     const container = this.$refs.visualization;
     const items = new DataSet(this.items);
     const groups = new DataSet(this.groups);
-    this.graph2d = new Graph2d(container, items, groups, this.options);
+    graph2d = new Graph2d(container, items, groups, this.options);
     events.forEach(eventName =>
-      this.graph2d.on(eventName, props => this.$emit(eventName, props))
+      graph2d.on(eventName, props => this.$emit(eventName, props))
     );
     if (this.withTimeTick) {
       this.timeline.on('currentTimeTick', props =>
@@ -128,14 +126,7 @@ export default {
     }
   },
   beforeDestroy() {
-    events.forEach(eventName =>
-      this.graph2d.off(eventName, props => this.$emit(eventName, props))
-    );
-    if (this.withTimeTick) {
-      this.timeline.off('currentTimeTick', props =>
-        this.$emit('currentTimeTick', props)
-      );
-    }
+    graph2d.destroy();
   }
 };
 </script>
