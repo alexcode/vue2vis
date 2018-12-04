6c425d9713245d916ac5e5504c8722a4df9dd42f
Alex Couturon <infocontact.alex@gmail.com>
2018-12-04 15:47:56 +0100

    Update packages

--- examples/build/webpack.prod.conf.js
+++ examples/build/webpack.prod.conf.js
@@ -14,6 +14,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const env = require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
